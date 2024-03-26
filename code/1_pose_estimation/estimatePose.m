3d0b7239f38f0009ae8fb75d3e02419f5ff3a201
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-03-26 12:50:24 -0400

    Managed indentations and code formatting

--- code/1_pose_estimation/estimatePose.m
+++ code/1_pose_estimation/estimatePose.m
@@ -20,9 +20,68 @@ function [position, orientation] = estimatePose(data, t)
     % orientation = euler angles representing the orientation of the
     % drone(body) in the world frame in the current time, in the order ZYX
 
-% MY IMPLEMENTATION START -------------------------------------------------
-    
-    position = zeros(3,1);
-    orientation = zeros(3,1);
-% MY IMPLEMENTATION END ---------------------------------------------------
+%% MY IMPLEMENTATION START ------------------------------------------------
+% tic
+
+k = [311.0520, 0, 201.8724; 0, 311.3885, 113.6210; 0, 0, 1];
+% R = eye(3,3);
+% R_temp = eye(3,3);
+% T = eye(3,1);
+% T_temp = eye(3,1);
+for i = 1:length(data(t).id)
+    pw = getCorner(data(t).id(i));
+    pc = [data(t).p0(:,i), data(t).p1(:,i), data(t).p2(:,i), data(t).p3(:,i), data(t).p4(:,i)];
+    for j = 2:length(pw)
+        if (j == 2)
+            A = [pw(1,j), pw(2,j), 1, 0, 0, 0, (-pc(1,j)*pw(1,j)), (-pc(1,j)*pw(2,j)), (-pc(1,j));
+                0, 0, 0, pw(1,j), pw(2,j), 1, (-pc(2,j)*pw(1,j)), (-pc(2,j)*pw(2,j)), (-pc(2,j))];
+        else
+            A = vertcat(A, [pw(1,j), pw(2,j), 1, 0, 0, 0, (-pc(1,j)*pw(1,j)), (-pc(1,j)*pw(2,j)), (-pc(1,j));
+                0, 0, 0, pw(1,j), pw(2,j), 1, (-pc(2,j)*pw(1,j)), (-pc(2,j)*pw(2,j)), (-pc(2,j))]);
+        end
+    end
+    [~, ~, V] = svd(A);
+    h = sign(V(9,9))*[V(1:3,9), V(4:6,9), V(7:9,9)];
+    % RT = k \ h;
+    % R1 = RT(:,1);
+    % R1_hat = [0, -R1(3,1), R1(2,1); R1(3,1), 0, -R1(1,1); -R1(2,1), R1(1,1), 0];
+    % R2 = RT(:,2);
+    % R2_hat = [0, -R2(3,1), R2(2,1); R2(3,1), 0, -R2(1,1); -R2(2,1), R2(1,1), 0];
+    % T = RT(:,3);
+    % T_hat = [0, -T(3,1), T(2,1); T(3,1), 0, -T(1,1); -T(2,1), T(1,1), 0];
+    % [Ur, ~, Vr] = svd(cross((R1_hat * R2_hat * R1_hat), R2_hat));
+    % if i == 1
+    %     R = Ur * [1, 0, 0; 0, 1, 0; 0, 0, det(Ur*Vr')] * Vr';
+    %     T_temp = T_hat / norm(R1_hat);
+    %     T = [T_temp(3,2); -T_temp(3,1); T_temp(2,1)];
+    % else
+    %     R_temp = Ur * [1, 0, 0; 0, 1, 0; 0, 0, det(Ur*Vr')] * Vr';
+    %     R = leastSquaresAvg(R, R_temp);
+    %     T_temp = T_hat / norm(R1_hat);
+    %     T_temp = [T_temp(3,2); -T_temp(3,1); T_temp(2,1)];
+    %     T = leastSquaresAvg(T, T_temp);
+    % end
+
+end
+
+RT = k \ h;
+R1 = RT(:,1);
+R1_hat = [0, -R1(3,1), R1(2,1); R1(3,1), 0, -R1(1,1); -R1(2,1), R1(1,1), 0];
+R2 = RT(:,2);
+R2_hat = [0, -R2(3,1), R2(2,1); R2(3,1), 0, -R2(1,1); -R2(2,1), R2(1,1), 0];
+T = RT(:,3);
+T_hat = [0, -T(3,1), T(2,1); T(3,1), 0, -T(1,1); -T(2,1), T(1,1), 0];
+[Ur, ~, Vr] = svd(cross((R1_hat * R2_hat * R1_hat), R2_hat));
+
+R = Ur * [1, 0, 0; 0, 1, 0; 0, 0, det(Ur*Vr')] * Vr';
+T_temp = T_hat / norm(R1_hat);
+T = [T_temp(3,2); -T_temp(3,1); T_temp(2,1)];
+% disp("DEBUG POINT")
+% [Ur, ~, Vr] = svd(cross((R1 * R2 * R1), R2));
+% R = Ur * [1, 0, 0; 0, 1, 0; 0, 0, det(Ur*Vr')] * Vr';
+position = T;
+orientation = rotm2eul(R);
+
+% toc
+%% MY IMPLEMENTATION END --------------------------------------------------
 end
